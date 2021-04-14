d6f66687908ae8ad390604338f167efc88f049c9
FOSS4Everyone <FOSS4Everyone@tutanota.com>
2021-04-14 20:49:09 +0200

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,17 @@
 def main():
   import random
-  dice_rolls = 2
+  dice_rolls = int(input('how many dice would you like to roll?'))
+  dice_size = int(input('how many sides are the dice'))
   dice_sum = 0
   for i in range(0,dice_rolls): #"for every index in a range starting after 0 until the value is equal to dice_rolls, do this:"
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum = dice_sum + roll
-    print(f'you rolled a {roll}')
+    if roll == 1:
+        print(f'you rolled a {roll}! critical fail')
+    elif roll == dice_size:
+        print(f'your rolled a {roll}! congrats')
+    else:
+        print(f'you rolled a {roll}')
   print(f"You rolled a total of {dice_sum}")
 
 if __name__== "__main__":
